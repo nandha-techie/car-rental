17c24ffbfe18b53fcfd0a69725c03f590ab8e22c
nandha-techie <nandhawebapps@gmail.com>
2023-09-12 17:59:14 +0530

    fontawesome fixed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter, Route, Routes } from 'react-router-dom';
+import {HashRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './component/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -12,17 +12,17 @@ import './App.scss';
 function App() {
   return (
     <>
-    <BrowserRouter basename={'/car-rental/'} >
+    <HashRouter >
       <Navbar />
       <Routes>
-        <Route index path="/" element={<Home />} />
+        <Route exact path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/vehicle" element={<Vehicle />} />
         <Route path="/testimonial" element={<Testimonial />} />
         <Route path="/team" element={<Team />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 }
